8549a707c26e3c9b1919ecbcb25f1e2ed6acdb6b
David Dunlevy <david.dunlevy@gmail.com>
2023-03-04 14:35:30 +1300

    update

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.skilldistillery.film.data.FilmDAO;
+import com.skilldistillery.film.entities.Film;
 
 @Controller
 public class FilmController {
@@ -18,6 +19,9 @@ public class FilmController {
 	
 	@RequestMapping(path= {"/","home.do"})
 	public String goHome(Model model) {
+		Film TEST = filmDao.findFilmById(1); //Debug
+		model.addAttribute("TestFilm", TEST);
+		
 		return "home";
 	}
 }
--- src/main/java/com/skilldistillery/film/data/FilmDAO.java
+++ src/main/java/com/skilldistillery/film/data/FilmDAO.java
@@ -11,7 +11,7 @@ public interface FilmDAO {
 	public List<Actor> findActorsByFilmId(int filmId);
 	public Film findFilmById(int filmId);
 	public List<Film> findFilmByKeyWord(String userInput);
-	Film createFilm(Film film);
-	boolean deleteFilm(int filmId);
-	Film updateFilm(int filmId, Film film);
+	public Film createFilm(Film film);
+	public boolean deleteFilm(int filmId);
+	public Film updateFilm(int filmId, Film film);
 }
